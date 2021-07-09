dbd78f42d6d69f195fe78a3ad8bca2cbde62ddf1
Tommy Lin <aks60808@gmail.com>
2021-07-09 10:15:19 +0800

    resolved code block at pf-US

--- _functions/pf_us.py
+++ _functions/pf_us.py
@@ -15,29 +15,29 @@ def worker(command):
     
     filename = command[0]
     output_path_pf = command[1]
-    f = "datasets\\" + filename + '.xls'
-    output_pf = output_path_pf +'/'+ 'pf_' + filename + '.xls'
+    f = "datasets\\" + filename + '.xlsx'
+    output_pf = output_path_pf +'/'+ 'pf_' + filename + '.xlsx'
     df = pd.read_excel(f,header=[0,1])
     lst = [x for x in df.columns]
     lst = [ y for _,y in lst]
     df.columns = lst
     df = df[(df['ES'] == 133)| (df['ES'] == 158)]
     df = df[(df['Cyc#'] != 0)]
-    df = df[['StepTime(s)', 'Temp 1']]
-    df['Run time(min)'] = df['StepTime(s)'].divide(60)
+    df = df[['Step (Sec)', 'Aux #1']]
+    df['Run time(min)'] = df['Step (Sec)'].divide(60)
 
     # fomat the values 
     df['Run time(min)'] = df['Run time(min)'].astype(float)
     df['Run time(min)'] = df['Run time(min)'].round(4)
     # each row of Step Time was divided by the first value in the column
-    df['Retention(%)'] = df['StepTime(s)'].divide(df['StepTime(s)'].iloc[0])
+    df['Retention(%)'] = df['Step (Sec)'].divide(df['Step (Sec)'].iloc[0])
     # format the value
     df['Retention(%)'] = df['Retention(%)'].astype(float)
     df['Retention(%)'] = df['Retention(%)'].round(4)
     # rearrange the order of columns
-    df_cols = ['StepTime(s)','Run time(min)','Temp 1','Retention(%)']
+    df_cols = ['Step (Sec)','Run time(min)','Aux #1','Retention(%)']
     df = df[df_cols]
-    df = df.rename(columns={"Temp 1": "Temperature(°C)"})
+    df = df.rename(columns={"Aux #1": "Temperature(°C)"})
     df = df.reset_index()
     df = df.drop(columns=['index'])
     df.to_excel(output_pf,index=False)
@@ -68,12 +68,12 @@ def run_pf_us():
                 os.mkdir(output_path_subfolder_pf)
             except OSError:
                 pass
-            print("******Now processing: %s" % entry.name)
             print("%d : %s" % (i,entry.name))
             command =[filename,output_path_subfolder_pf]
             proc_list.append(command)
+           
             i += 1
-        print("****** Total %d files will be processed in parallel. ******" % i)
+    print("****** Total %d files will be processed in parallel. ******" % i)
     multiple_results = pool.map_async(worker,proc_list)
     pool.close()
     animation = "|/-\\"
