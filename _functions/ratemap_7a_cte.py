6c47e5930879bfd340fc0cc85cf3f2a65ef6f61b
Tommy Lin <aks60808@gmail.com>
2021-09-06 12:28:15 +0800

    add 7a cte

--- _functions/ratemap_7a_cte.py
+++ _functions/ratemap_7a_cte.py
@@ -12,43 +12,35 @@ def worker(command):
     
     file = command[0]
     filename  = command[1]
-    output_path_7a_us = command[2]
-    excel_flag = int(command[3])
-    proc_mode = command[4]
+    output_path_7a_cte = command[2]
     f = "datasets\\" + file
-    output_7a_us = output_path_7a_us +'/'+ '7A_US_' + filename + '.xlsx'
-    if excel_flag == 1:
-        df = pd.read_excel(f,header=1)
+    output_7a_cte = output_path_7a_cte +'/'+ '7A_CTE_' + filename + '.xlsx'
+    df = pd.read_excel(f,header=2)
 
-    else:
-        df = pd.read_csv(f,sep='\t',header=1)
-    # lst = [x for x in df.columns]
-    # lst = [ y for y in lst]
-    # df.columns = lst
-    df = df[df['State']=='D']
-    df_cyc_sum = df[(df['ES'] == 133)| (df['ES'] == 158)]
-    workbook = xlsxwriter.Workbook(output_7a_us)
+    df = df[((df['Action']=='CC\D')|(df['Action']=='CP\D'))&(df['R']!=float('inf'))]
+    df_cyc_sum = df[(df['End Status']=='EV')|(df['End Status']=='ET')]
+    workbook = xlsxwriter.Workbook(output_7a_cte)
     Step_list = list( int(i) for i in df_cyc_sum['Step'])
     worksheet = workbook.add_worksheet()
 
     ind = 0
     for step in Step_list:
         df_temp = df[df['Step'] == step]
-        df_temp['Amps_change'] = ((df_temp['Amps'] - df_temp['Amps'].iloc[0])*100).abs()
-        df_temp['Watt-hr'] = df_temp['Watt-hr'] * 1000
-        df_temp['Amp-hr'] = df_temp['Amp-hr'] * 1000
-        df_temp['Power'] = df_temp['Amps'] * df_temp['Volts']
+        df_temp['Amps_change'] = ((df_temp['I'] - df_temp['I'].iloc[1])*100).abs()
+        df_temp['Watt-hr'] = df_temp['WH'] 
+        df_temp['Amp-hr'] = df_temp['mAH'] 
+        df_temp['Power'] = df_temp['P'] 
 
         ## constant current mode
-        if (df_temp['Amps_change']<10).all() == True:
+        if (df_temp['Amps_change']<15).all() == True:
     
-            format_Amps = "{:.2f}".format(df_temp['Amps'].iloc[1])
-            Temperature_list = list(df_temp['Aux #1'])
+            format_Amps = "{:.2f}".format(df_temp['I'].iloc[1])
+            Temperature_list = list(df_temp['T'])
             Temperature_list.insert(0, f'{format_Amps}A')
             Temperature_list.insert(0, f'Step-{step}')
             Temperature_list.insert(0, 'T')
             
-            Volts_list = list(df_temp['Volts'])
+            Volts_list = list(df_temp['V'])
             Volts_list.insert(0, f'{format_Amps}A')
             Volts_list.insert(0, f'Step-{step}')
             Volts_list.insert(0, 'V')
@@ -62,13 +54,13 @@ def worker(command):
                 worksheet.write(j, ind+1, Volts_list[j])
                 worksheet.write(j, ind+2, Temperature_list[j])
         else:
-            format_Power = round(df_temp['Power'].iloc[1])
-            Temperature_list = list(df_temp['Aux #1'])
+            format_Power = round(df_temp['Power'].iloc[-1])
+            Temperature_list = list(df_temp['T'])
             Temperature_list.insert(0, f'{format_Power}W') 
             Temperature_list.insert(0, f'Step-{step}')
             Temperature_list.insert(0, 'T')
             
-            Volts_list = list(df_temp['Volts'])
+            Volts_list = list(df_temp['V'])
             Volts_list.insert(0, f'{format_Power}W') 
             Volts_list.insert(0, f'Step-{step}' )
             Volts_list.insert(0, 'V')
@@ -86,20 +78,20 @@ def worker(command):
     workbook.close()
     
 
-def run_7a_us():
+def run_7a_cte():
     pool=multiprocessing.Pool()
     proc_list = []
     i = 1
-    proc_questions = [
-    inquirer.List('Mode',
-                    message="Discharge List",
-                    choices=['AUX', 'TEMP(not available)'],
-                ),
-    ]
-    proc_mode = inquirer.prompt(proc_questions)['Mode']
+    # proc_questions = [
+    # inquirer.List('Mode',
+    #                 message="Discharge List",
+    #                 choices=['AUX', 'TEMP(not available)'],
+    #             ),
+    # ]
+    # proc_mode = inquirer.prompt(proc_questions)['Mode']
     cur_dir = os.getcwd()
     datafolder=cur_dir + '\datasets'
-    output_path_root_folder = cur_dir + "/7A_US_output"
+    output_path_root_folder = cur_dir + "/7A_CTE_output"
     try:
         os.mkdir(output_path_root_folder)
     except OSError:
@@ -109,18 +101,14 @@ def run_7a_us():
         
         filename  = Path(entry.path).stem
         output_path_folder = output_path_root_folder + '/'+ filename
-        output_path_subfolder_pf = output_path_folder + '/' + "7A_US"
+        output_path_subfolder_pf = output_path_folder + '/' + "7A_CTE"
         try:
             os.mkdir(output_path_folder)
             os.mkdir(output_path_subfolder_pf)
         except OSError:
             pass
         print("%d : %s" % (i,entry.name))
-        if entry.path.endswith(".xlsx") or entry.path.endswith(".xls"):
-            xlsx_flag = 1
-        else:
-            xlsx_flag = 0
-        command =[entry.name,filename,output_path_subfolder_pf,xlsx_flag,proc_mode]
+        command =[entry.name,filename,output_path_subfolder_pf]
         proc_list.append(command)
         
         i += 1
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from _functions.dcir import run_dcir
 from _functions.bdcir_ratemap import run_bdcir_ratemap
 from _functions.helper import welcome
 from _functions.ratemap_7a_us import run_7a_us
+from _functions.ratemap_7a_cte import run_7a_cte
         
         
     
@@ -21,7 +22,7 @@ if __name__ == '__main__':
     mode_prompt = [
     inquirer.List('Mode',
                     message="Service List",
-                    choices=['PF+CD+HF+BDCIR(CPD only)', 'DCIR', 'PF(US)', 'BDCIR+RATEMAP','7a(US)'],
+                    choices=['PF+CD+HF+BDCIR(CPD only)', 'DCIR', 'PF(US)', 'BDCIR+RATEMAP','7a'],
                 ),
     ]
     service_mode = inquirer.prompt(mode_prompt)['Mode']
@@ -34,8 +35,18 @@ if __name__ == '__main__':
         run_pf_us()
     elif service_mode == 'BDCIR+RATEMAP':
         run_bdcir_ratemap()
-    elif service_mode == '7a(US)':
-        run_7a_us()
+    elif service_mode == '7a':
+        machine_prompt = [
+        inquirer.List('Mode',
+                        message="Service List",
+                        choices=['US','CTE'],
+                    ),
+        ]
+        machine_option = inquirer.prompt(machine_prompt)['Mode']
+        if machine_option == 'US':
+            run_7a_us()
+        else:
+            run_7a_cte()
     print(f"{service_mode} has been completed, exit...")
 
 
